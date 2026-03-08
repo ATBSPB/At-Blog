4cb39de27bd258ced34a1ef14379ae0e985d0a49
At <atbspb@qq.com>
2026-03-08 14:19:15 +0800

    Initial commit: 重建项目

---
+++ src/config.ts
@@ -0,0 +1,153 @@
+import type {
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+/**
+ * 站点配置
+ * 包含网站的基本信息和全局设置
+ */
+export const siteConfig: SiteConfig = {
+	// 网站标题
+	title: "At - Blog",
+	// 网站副标题
+	subtitle: "ATBSPB个人博客",
+	// 网站语言代码，例如 'en', 'zh_CN', 'ja' 等
+	lang: "zh_CN",
+	// 主题颜色设置
+	themeColor: {
+		// 主题颜色的默认色相值，范围从 0 到 360
+		// 例如：红色: 0, 青色: 200, 蓝绿色: 250, 粉色: 345
+		hue: 275,
+		// 是否固定主题颜色，隐藏主题颜色选择器
+		fixed: false,
+	},
+	// 网站横幅设置
+	banner: {
+		// 是否启用横幅
+		enable: true,
+		// 横幅图片路径
+		// 相对于 /src 目录，以 '/' 开头则相对于 /public 目录
+		src: "assets/images/demo-banner.png",
+		// 横幅图片位置，支持 'top', 'center', 'bottom'
+		position: "bottom",
+		// 横幅图片 credits
+		credit: {
+			// 是否显示 credits
+			enable: true,
+			// credits 文本
+			text: "相册",
+			// (可选) 原始作品或艺术家页面的 URL 链接
+			url: "https://photo.atbspb.online",
+		},
+	},
+	// 目录设置
+	toc: {
+		// 是否在文章右侧显示目录
+		enable: true,
+		// 目录显示的最大标题深度，范围从 1 到 3
+		depth: 2,
+	},
+	// 网站图标设置
+	// 留空数组使用默认图标
+	favicon: [
+		{
+			src: "/favicon/logo.png",
+			sizes: "any",
+		},
+	],
+};
+
+/**
+ * 导航栏配置
+ * 定义网站导航菜单的链接
+ */
+export const navBarConfig: NavBarConfig = {
+	links: [
+		// 使用预设的首页链接
+		LinkPreset.Home,
+		// 使用预设的归档链接
+		LinkPreset.Archive,
+		// 使用预设的关于页面链接
+		LinkPreset.About,
+		// 自定义外部链接
+		{
+			// 链接名称
+			name: "GitHub",
+			// 链接 URL
+			// 内部链接不应包含基础路径，会自动添加
+			url: "https://github.com/ATBSPB",
+			// 是否为外部链接（显示外部链接图标并在新标签页打开）
+			external: true,
+		},
+	],
+};
+
+/**
+ * 个人资料配置
+ * 定义网站个人资料相关信息
+ */
+export const profileConfig: ProfileConfig = {
+	// 头像图片路径
+	// 相对于 /src 目录，以 '/' 开头则相对于 /public 目录
+	avatar: "assets/images/demo-avatar.png",
+	// 姓名
+	name: "ATBSPB",
+	// 个人简介
+	bio: "The only way to do great  is to love what you do.",
+	// 社交链接
+	links: [
+		{
+			// 链接名称
+			name: "邮箱",
+			// 图标代码，访问 https://icones.js.org/ 获取
+			// 需要安装相应的图标集：`pnpm add @iconify-json/<icon-set-name>`
+			icon: "mdi:email-outline",
+			// 链接 URL
+			url: "mailto:atbspb@qq.com",
+		},
+		{
+			name: "Steam",
+			icon: "fa7-brands:steam-symbol",
+			url: "https://steamcommunity.com/id/atbspb/",
+		},
+		{
+			name: "抖音",
+			icon: "line-md:tiktok",
+			url: "https://v.douyin.com/4AZ_D6aojl0/",
+		},
+		{
+			name: "bilibili",
+			icon: "tabler:brand-bilibili",
+			url: "https://space.bilibili.com/3493117248407854",
+		},
+	],
+};
+
+/**
+ * 许可证配置
+ * 定义网站内容的许可证信息
+ */
+export const licenseConfig: LicenseConfig = {
+	// 是否启用许可证显示
+	enable: false,
+	// 许可证名称
+	name: "CC BY-NC-SA 4.0",
+	// 许可证 URL
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+/**
+ * 代码高亮配置
+ * 配置 Expressive Code 的行为
+ */
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// 代码主题
+	// 注意：部分样式（如背景色）在 astro.config.mjs 文件中被覆盖
+	// 请选择深色主题，因为此博客主题目前仅支持深色背景
+	theme: "github-dark",
+};
---
+++ src/content/posts/guide/index.md
@@ -0,0 +1,23 @@
+---
+title: 第一篇文章!
+published: 2026-03-07
+description: 这是我写的第一篇博客！
+tags: [生活,记录]
+category: 日常
+draft: false
+---
+
+## 欢迎来到我的博客，这是我的第一篇文章!
+
+#### 说说我学开发的艰苦历程吧，过程大概是这样的：  
+
+1. 好像大概是25年的8月初,在放暑假嘛，闲的慌，游戏不想打，视频不想刷，但是手又莫名其妙地打开了b站。刚好，看到了一个up主的视频，应该是<a href="https://www.bilibili.com/video/BV16A4y1X7vg/?share_source=copy_web&vd_source=9e71c69c09bc976471a5c4a56f257a5a" target="_blank" rel="noopener noreferrer">这个</a>。说怎么搭建自己的网站的，当时还一点关于web的概念都没有，但有个教程嘛，就稀里糊涂地一步一步跟着做。
+那时候没什么钱，也不知道什么小厂，又不舍得去阿里腾讯买天价服务器，刚好看到他视频里说的本地建站，就在直接在电脑上下了个windows的宝塔，到后面才知道连最基本的公网IP和域名都没有，怎么可能建好。现在想起来还是挺好笑的，忙活了几天还是什么也没有，但也不怎么感觉气馁，还在那傻乐。
+
+2. 后面又是在B站上，看到有up主在说有免费的服务器可以用，就去<a href="http://cpanel.hostingem.ru" target="_blank" rel="noopener noreferrer">这儿</a>申请了一个。拿到手了以后又欢天喜地地跑去各种教学视频底下看，可还是第一步就卡住了，说要先登录终端，但把整个控制面板翻遍了，我也没找到IP和密码。后来才知道，那只是台虚拟主机。捣鼓了几天，终于误打误撞地把WordPress装好了，虽然只是一个模板,但是第一次打开自己的网站，那股兴奋劲现在想起来也还是挺欣慰的吧。
+
+3. 再后面，我在一个网站上看到了一个提供公益虚拟主机的项目，才分清楚虚拟主机和服务器的区别。在那里我才真正有了一个属于自己的网页，虽然html还是AI写的，但也是有了一点我自己的气息吧。这里我想附张图，是当初网站的样子，还是经典的单色渐变，确实是有点丑的。<a href="https://photo.atbspb.online/index.php?mod=io&op=getStream&path=dWpFSjVubmdHQXUxakZxMFpqMWc2UUpSUHZtX3dSREYxbEFZbmR1NWhzcU5MLURRMGUzVWhibUo2dlY4WUYwX2QyQmhjaENfNnBpMkRpbzA" target="_blank" rel="noopener noreferrer">截图在这</a>
+
+4. 在买下了第一台服务器后，我开始学基础的html和css了，当时看的<a href="https://www.bilibili.com/video/BV1kM4y127Li/?share_source=copy_web&vd_source=9e71c69c09bc976471a5c4a56f257a5a" target="_blank" rel="noopener noreferrer">黑马程序员</a>的视频，讲的很详细，应该也是许多新手站长的启蒙老师。看完后马上就忍不住去实操了，自己写了一个个人主页，但还是很粗糙，样式也还是AI写的。各种背景和图标什么的都是用的mp4和jpg,直接干到了三十多M，加载巨慢。但是进步还是挺大的。那个主页我一直用了三四个月，现在还是挺怀念的，就放张<a href="https://photo.atbspb.online/index.php?mod=io&op=getStream&path=dWpFSjVubmdHQXUxakZibU1Udy02d1FNWXZycWswZVIxRnBPbTQtNWc4N2ZjYjNXMExfUzJMMkl0UDkxTkExaGQyWXpmRWJ0N2NpLUFTbzA" target="_blank" rel="noopener noreferrer">图片</a>纪念一下吧。
+
+5. 过了新手期后，也就没什么好说的了，一直在学各种语言和框架，但懒得手搓了，就找别人的来魔改，就这样磨磨蹭蹭地做了大半年，才成了现在的这个样子。现在是高二下学期了，高中三年已经过了大半，时间过得真的挺快，虽然理想的学校是<a href="https://nuaa.edu.cn/" target="_blank" rel="noopener noreferrer">南航</a>，也很期待大学生活，但是我还是有点不舍得一中，舍不得那些人和事，可总是要向前看的嘛。现在是晚上两点，我好像是十二点开始写的吧，删删又改改，效率确实是有点低了，但是到最后还是一堆废话。有点偏题了哈，其实还有很多很多没有写出来的，但还是就到这里吧。
